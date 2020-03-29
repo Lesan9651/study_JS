52c9ece172f7cdd3c84d9521424974ca74e77a1a
Aleksandr Kirienkov <shurakirienkov@yandex.ru>
2020-03-29 15:48:16 +0300

    Типы данных, операторы, методы и свойства

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let money = 30000;
+console.log(money);
+
+let income = '50000';
+console.log(income);
+
+let addExpenses = '800, 400, 7000';
+
+let str = 'addExpenses';
+console.log(str.length);
+console.log(str.toLowerCase());
+
+console.log(str[0]);
+console.log(str[1]);
+console.log(str[2]);
+console.log(str[3]);
+console.log(str[4]);
+console.log(str[5]);
+console.log(str[6]);
+console.log(str[7]);
+console.log(str[8]);
+console.log(str[9]);
+console.log(str[10]);
+console.log(str[11]);
+
+let deposit = true;
+console.log(deposit);
+
+let mission = 10000000;
+console.log(mission);
+
+let period = 12;
+console.log(period);
+
+let budgetDay = 25000;
+console.log(budgetDay);
